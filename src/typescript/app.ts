78730ead0d69527f31a97c30ccadb39edfb6d3ac
lexxyar <lexxyar@ya.ru>
2018-09-18 23:47:40 +0300

    config coping

--- src/typescript/app.ts
+++ src/typescript/app.ts
@@ -1,14 +1,1 @@
 import '../css/app.scss';
-
-export class C {
-    private x = 10
-    getX = () => this.x;
-    setX = (newVal: number) => { this.x = newVal; }
-}
-
-export let x = new C();
-export let y = { ...{ some: "value" } }
-
-import { a } from './next';
-
-console.log(a.sum(2, 3).toString());
--- src/typescript/next.ts
+++
@@ -1,6 +0,0 @@
-export class A{
-    sum(a: number, b: number): number{
-        return a + b;
-    }
-}
-export let a = new A();
