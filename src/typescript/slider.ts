4bf14414006f6a6ef15654db0e641dae73179794
lexxyar <lexxyar@ya.ru>
2018-09-22 21:36:34 +0300

    temp commit

--- src/typescript/slider.ts
+++ src/typescript/slider.ts
@@ -6,19 +6,30 @@ export class Slider {
   private _obj: HTMLElement;
   private _timer: number;
   private _conf: SliderConfig;
-  private _items: NodeList;
+  private _navItems: NodeList;
+  private _slideItems: NodeList;
   private _activeItem: number;
-  static a:number = 1;
+  static a: number = 1;
   public constructor(private _selector: string, private _config: object = {}) {
     Slider.a++;
     console.log(Slider.a);
     this._obj = document.querySelector(_selector) as HTMLElement;
-    this._items = this._obj.querySelectorAll(".item");
-    this._activeItem = 1;
-    let e = this._items[this._activeItem - 1] as HTMLElement;
-    e.classList.toggle("active");
-    console.log(e.classList);
-    // (this._items[this._activeItem - 1] as HTMLElement).classList.toggle('active');
+    this._activeItem = 0;
+    this._navItems = this._obj.querySelectorAll(".navpg ul li") as NodeList;
+    this._slideItems = this._obj.querySelectorAll(".items .item") as NodeList;
+    this.setActiveNav();
+    for (let i = 0; i < this._navItems.length; i++) {
+      let liItem = (this._navItems[i] as HTMLElement).querySelector(
+        "a"
+      ) as HTMLElement;
+      let that = this;
+      liItem.addEventListener("click", e => {
+        e.preventDefault();
+        let slideNum = new Number((e.target as HTMLElement).dataset.slide);
+        that._activeItem = slideNum.valueOf();
+        that.setActiveNav();
+      });
+    }
 
     this._conf = new SliderConfig();
     this.fillConfig();
@@ -32,4 +43,22 @@ export class Slider {
   }
 
   private fillConfig() {}
+
+  private setActiveNav() {
+    for (let i = 0; i < this._navItems.length; i++) {
+      let liItem = (this._navItems[i] as HTMLElement).querySelector(
+        "a"
+      ) as HTMLElement;
+      let classList = liItem.classList;
+      if (this._activeItem === i) {
+        if (!classList.contains("active")) {
+          classList.add("active");
+        }
+      } else {
+        if (classList.contains("active")) {
+          classList.remove("active");
+        }
+      }
+    }
+  }
 }
